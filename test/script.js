3495f384a2567e6fed8eb0239811f2f052f11f4e
hybridpolaris <trinhquanghuy2701@gmail.com>
2025-11-18 14:13:26 +0700

    i forgot to commit, basically this is a half-implementation of the test

--- test/script.js
+++ test/script.js
@@ -1,3 +1,5 @@
+const enableAI = true;
+
 var selected = localStorage.getItem("theme");
 selected = selected ?? "light";
 if (selected == "dark") {
@@ -11,11 +13,13 @@ if (selected == "dark") {
     document.body.className = "";
   }
 }
+
 const translationKeys = {
-  ielts_gen: "IELTS",
-  ielts_acad: "IELTS Academic",
+  ielts_gen: "General Training IELTS",
+  ielts_acad: "Academic IELTS",
   toeic: "TOEIC",
 };
+
 const params = new URLSearchParams(window.location.search);
 const testType = params.get("type");
 const testIncludes = params.getAll("include");
@@ -24,19 +28,19 @@ document.title = `${translationKeys[testType]} practice - Writemaster`;
 let timeLeft = 0; // seconds
 let intervalId = 0; // what
 const TimerElement = document.getElementById("timer");
-function StartTimer(time) {
+function startTimer(time) {
   timeLeft = Date.now() + 1000 * time;
-  intervalId = setInterval(UpdateTimer, 100);
+  intervalId = setInterval(updateTimer, 100);
 }
 
-function UpdateTimer() {
+function updateTimer() {
   // please make variable names more self explanatory :(
   let et = Math.ceil((timeLeft - Date.now()) / 1000);
   if (et <= 0) {
     clearInterval(intervalId);
     TimerElement.innerHTML = "0:00";
     TimerElement.style = /*css*/ "color:var(--accent-color)";
-    Submit();
+    submit();
     return;
   } else {
     TimerElement.style = "";
@@ -54,19 +58,24 @@ function UpdateTimer() {
     )}:${seconds.padStart(2, "0")}`;
   }
 }
+
 document.getElementById("submit_btn").addEventListener("click", () => {
-  clearInterval(intervalId);
-  TimerElement.style = /*css*/ "color:var(--accent-color)";
-  Submit();
+  // asking for confirmation
+  window.confirm('Are you sure you want to submit?') && submit();
 });
-function Submit() {
+
+function submit() {
   //tbh idk what should be done here
   //leave that for qh
+  TimerElement.style = /*css*/ "color:var(--accent-color)";
+  clearInterval(intervalId);
   console.log("Test is done!");
-  alert("uiia");
+  alert("aqme");
 }
+
 async function getAIResponse(prompt = "") {
-  return `This is a response to the question "${prompt}"`;
+  if (!enableAI) return `This is a response to the question "${prompt}"`;
+
   const response = await fetch(
     `https://writemaster-api.vercel.app/api/ai?prompt=${encodeURIComponent(
       prompt
@@ -86,12 +95,33 @@ async function getAIResponse(prompt = "") {
   }
 
   const data = await response.json();
+  console.log(data);
   return data.candidates[0].content.parts[0].text;
 }
 
-/*getAIResponse(
-  `I'm practicing for ${testType}, can you generate a writing question for me? I don't want any tips/directions, as I'd like this to be a sort of mock test. \nNotes: you don't need to provide pictures or anything. Thanks.`
-).then((response) => {
-  console.log(response);
-});
-*/
+// CODE STARTS HERE
+// is hpol or aqme a better username/displayname (pls answer i need to pick)
+
+document.getElementById('title').innerText = `${translationKeys[testType].toUpperCase()} PRACTICE TEST`;
+if (testType != 'toeic') {
+  if (testIncludes.includes('writing')) {
+    const section1 = document.createElement('div');
+    const section1Title = document.createElement('h3');
+    const section1Question = document.createElement('p');
+    const section1Textbox = document.createElement('textarea');
+    
+    section1.className = 'section';
+    section1Title.innerText = 'Writing task 1';
+    getAIResponse(
+      `I'm practicing for ${translationKeys[testType]}, can you generate a writing part 1 question for me? I don't want any tips/directions, as I'd like this to be a sort of mock test. \nNotes: you only need to provide charts in ONLY image form (not text form with markdown) if it is academic IELTS and otherwise, you should ONLY ANSWER WITH THE EXAM QUESTION and nothing else (except thE chart IMAGE), not even a title like 'IELTS Writing part 2'; and also not use any markdown, just plain text. Thanks.`
+    ).then((response) => {
+      section1Question.innerText = response;
+    });
+    
+    section1.appendChild(section1Title);
+    section1.appendChild(section1Question);
+    section1.appendChild(section1Textbox);
+    document.getElementById('test').appendChild(section1);
+    startTimer(3600);
+  }
+}
